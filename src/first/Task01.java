18928e42a63f6a54640d89fb197ced2a106fb9a8
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-06-24 14:16:57 +0300

    ttt

--- src/first/Task01.java
+++ src/first/Task01.java
@@ -3,6 +3,7 @@ package first;
 public class Task01 {
 
     public static void main(String[] args) {
+        System.out.println("ttt");
         System.out.println("Hello, Saint Petersburg!");
     }
 }
