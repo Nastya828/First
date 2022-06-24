a79783e1451d2f3cabda999f120a976c544435cb
Anastasiia Iakimova <anastasiiaiakimova@MacBook-Pro.local>
2022-06-24 14:11:31 +0300

    Change Word on Saint Petersburg

--- src/first/Task01.java
+++ src/first/Task01.java
@@ -3,6 +3,6 @@ package first;
 public class Task01 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Saint Petersburg!");
     }
 }
